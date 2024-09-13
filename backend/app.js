8aa7f901d52948e9c56cb43770ff73ab25a6e3c7
vedali29 <vedalipawar29@gmail.com>
2024-09-13 22:53:59 +0530

    url changing

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const mongoURI = process.env.mongoURI;
 
